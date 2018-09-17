8811d3d150069468b0722cc58e55c1ef74521bb3
bs482 <bs482@pvsd.org>
2018-09-17 11:20:45 -0400

    imported code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String word1;
+        String word2;
+
+        System.out.println("Type word one:");
+        Scanner kb = new Scanner(System.in);
+        word1 = kb.next();
+
+        System.out.println("Type word two");
+        word2 = kb.next();
+
+        if(word1.length() != word2.length()){
+            System.out.println(word1 + " and " +  word2 + " are not anagrams!");
+        }
+        else{
+
+            char[] wordone = word1.toCharArray();
+            char[] wordtwo = word2.toCharArray();
+
+            Arrays.sort(wordone);
+            Arrays.sort(wordtwo);
+
+            if(Arrays.equals(wordone, wordtwo)) {
+                System.out.println(word1 + " and " + word2 + " are anagrams!");
+            }
+            else{
+                System.out.println(word1 + " and " + word2 + " are not anagrams!");
+            }
+        }
     }
 }
